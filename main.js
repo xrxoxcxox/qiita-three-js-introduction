eb2a4a41f0105a438e21017b2d851c023d7deec5
Keisuke Watanuki <watanuki@ju-zen.com>
2022-10-17 03:27:34 +0900

    Add Light, PhysicalMaterial, Stats, Helper and Controls

--- main.js
+++ main.js
@@ -1,76 +1,68 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Stats from "three/examples/jsm/libs/stats.module";
+
+const renderArea = document.getElementById("app");
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderArea.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-
-const light = new THREE.SpotLight()
-light.position.set(5, 5, 5)
-scene.add(light)
+scene.background = new THREE.Color(0xeeeeee);
 
 const camera = new THREE.PerspectiveCamera(
-  75,
+  30,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 2;
+camera.position.set(2, 3, 5);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-const renderer = new THREE.WebGLRenderer();
-renderer.physicallyCorrectLights = true;
-renderer.shadowMap.enabled = true;
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById("app").appendChild(renderer.domElement);
+const directionalLight1 = new THREE.DirectionalLight(0xffffff, 100);
+directionalLight1.position.set(-1, 1, 1);
+scene.add(directionalLight1);
+
+const directionalLight2 = new THREE.DirectionalLight(0x4097db, 50);
+directionalLight2.position.set(1, 0, -1);
+scene.add(directionalLight2);
+
+const ambientLight = new THREE.AmbientLight(0x000000);
+scene.add(ambientLight);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshPhysicalMaterial({
+  color: 0x55c500,
+  roughness: 0.1,
+  transmission: 1,
+  ior: 1.45,
+  reflectivity: 1,
+  thickness: 10,
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const stats = Stats();
+renderArea.appendChild(stats.dom);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+const gridHelper = new THREE.GridHelper(13, 21);
+scene.add(gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-
-const loader = new GLTFLoader();
-loader.load(
-  "models/monkey.glb",
-  function (gltf) {
-    gltf.scene.traverse(function (child) {
-      if (child.isMesh) {
-        const m = child;
-        m.receiveShadow = true;
-        m.castShadow = true;
-      }
-      if (child.isLight) {
-        const l = child;
-        l.castShadow = true;
-        l.shadow.bias = -0.003;
-        l.shadow.mapSize.width = 2048;
-        l.shadow.mapSize.height = 2048;
-      }
-    });
-    scene.add(gltf.scene);
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  (error) => {
-    console.log(error);
-  }
-);
-
-window.addEventListener("resize", onWindowResize, false);
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-}
+controls.target.set(0, 0, 0);
 
 function animate() {
   requestAnimationFrame(animate);
-  controls.update();
-  render();
-}
-
-function render() {
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
   renderer.render(scene, camera);
+  controls.update();
+  stats.update();
 }
 
 animate();
